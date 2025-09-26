8f69df6821012ec22f88896ca1d7233f4ccb5664
Aaron Zhang <aaron@weikai.net>
2025-09-25 21:27:36 -0400

    remove unnecessary auto imports

--- Main.java
+++ Main.java
@@ -1,7 +1,5 @@
-import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Currency;
 
 public class Main {
     public static void main(String[] args) {
