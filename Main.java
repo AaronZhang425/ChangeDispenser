90ca1e8f0b6f78b2ddcde7abc462dd78c964959e
Aaron Zhang <aaron@weikai.net>
2025-09-25 21:25:57 -0400

    add all whole US money denominations to money ArrayList

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.lang.reflect.Array;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Currency;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,12 +11,21 @@ public class Main {
         System.out.println("\n");
 
         ArrayList<CurrencyDenomination> money = new ArrayList<>();
-        money.add(new CurrencyDenomination(10, "ten dollar bill"));
+        money.add(new CurrencyDenomination(100, "one hundred dollar bills"));
+        money.add(new CurrencyDenomination(50, "fifty dollar bills"));
+        money.add(new CurrencyDenomination(20, "twenty dollar bills"));
+        money.add(new CurrencyDenomination(10, "ten dollar bills"));
+        money.add(new CurrencyDenomination(5, "five dollar bills"));
+        money.add(new CurrencyDenomination(2, "two dollar bills"));
+        money.add(new CurrencyDenomination(1, "one dollar bills"));
 
-
-
-        input.close();
+        input.close();    
         
-        
+    }
+
+    public static ArrayList<CurrencyDenomination> reverseMergeSort(
+        ArrayList<CurrencyDenomination> money
+    ) {
+        return money;
     }
 }
